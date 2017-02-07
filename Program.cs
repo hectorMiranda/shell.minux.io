f6227e6a068a80fe030951b57e0d7ae2e9bf1bdd
Hector Miranda <marcetux@gmail.com>
2017-02-06 20:43:47 -0800

    First runnable app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marcetux 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Marcetux 0.1");
+        }
+    }
+}
