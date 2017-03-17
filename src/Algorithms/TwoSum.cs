f4b01700c9d8aeec66abef13c2b2126735e9150b
Hector Miranda <marcetux@gmail.com>
2017-03-17 16:58:53 -0700

    Wrapping all algorithm utilities under the same command

---
+++ src/Algorithms/TwoSum.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Marcetux.Algorithms
+{
+
+
+    ///<summary>
+    ///Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    ///each input have exactly one solution and all elements must be unique.
+    ///</summary>
+    public class TwoSum
+    {
+       
+
+    }
+
+
+}
---
+++ src/Shell/UtilCommand.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Text;
+using Marcetux.Algorithms;
+using System.Linq;
+
+
+namespace Marcetux.Shell
+{
+    internal class UtilCommand : ICommand
+    {
+        public string Name => "util";
+        public string Description => "Lets ";
+        public string Help => "This algorithms can help to you to prepare for your next interview";
+
+///Expects following syntax: algo fibo params[]s
+        public string Execute(string[] args)
+        {
+            var result = String.Empty;
+            var utilToRun = args.Length > 0? args[0]: String.Empty;
+
+            switch(utilToRun){
+                case "fibo":
+                    result = RunFibo();
+                    break;
+                case "twoSum":
+                    result = RunTwoSum();
+                    break;
+                default:
+                    result = "util not found!";
+                    break; 
+            }
+
+            return result;
+        }
+
+
+        public string RunTwoSum()
+        {
+
+            return null;
+        }
+
+        public string RunFibo()
+        {
+            var result = new StringBuilder();
+
+            var generator = new FibonacciGenerator();
+
+           foreach(var digit in generator.Generate(15))
+                result.Append($" {digit} ");
+            
+            return result.ToString();
+
+        }
+        
+    }
+}
