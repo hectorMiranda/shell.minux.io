f99613daaf887928ab407288217fbaea3564d0f9
Hector Miranda <marcetux@gmail.com>
2017-06-15 07:48:04 -0700

    adding echo API

--- src/Web/Modules/HomeModule.cs
+++ src/Web/Modules/HomeModule.cs
@@ -7,6 +7,22 @@ namespace Marcetux.Web
         public HomeModule()
         {
             Get("/", args => "Minux Server 0.0.1");
+
+            Get("/echo", _ =>
+            {
+                var echo = new
+                {
+                    Server = "Minux Server 0.0.1 says",
+                    this.Request.Headers,
+                    this.Request.Query,
+                    this.Request.Form,
+                    this.Request.Method,
+                    this.Request.Url,
+                    this.Request.Path
+                };
+
+                return Response.AsJson(echo);
+            });
         }
     }
 }
